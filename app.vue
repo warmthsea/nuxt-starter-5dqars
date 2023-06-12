2b1b8baa70ab612b2d5f3c0bec2bb3c5062a6f10
warmthsea <45450994+warmthsea@users.noreply.github.com>
2023-06-12 10:14:53 +0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    123
+    <!-- <NuxtWelcome /> -->
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    234
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import { num } from './index';
+</script>
+
+<template>
+  <div>index pages {{ num }}</div>
+</template>
